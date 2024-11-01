140e7b9e048926d57a7125448376666e0974505c
simrandabas <simrandabas1010@gmail.com>
2024-11-01 12:32:58 +0530

    Add files via upload

---
+++ Insightfolio/script.js
@@ -0,0 +1,12 @@
+// JavaScript for toggle functionality
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.querySelector(".menu-icon");
+    const nav = document.querySelector("nav");
+
+    function toggleMenu() {
+        const nav = document.querySelector('nav');
+        nav.classList.toggle('active');
+    }
+    
+    
+});
